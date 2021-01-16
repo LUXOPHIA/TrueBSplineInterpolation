a41a250a2de489da1d03066a1cbe4a2732c539c1
luxidea <contact@luxidea.net>
2021-01-17 02:50:27 +0900

    begin

---
+++ BSplineInterpolation.dpr
@@ -0,0 +1,44 @@
+﻿program BSplineInterpolation;
+
+
+
+
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Main in 'Main.pas' {Form1},
+  LUX in '_LIBRARY\LUXOPHIA\LUX\LUX.pas',
+  LUX.D1 in '_LIBRARY\LUXOPHIA\LUX\LUX.D1.pas',
+  LUX.D2 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2.pas',
+  LUX.D3 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.pas',
+  LUX.D4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.pas',
+  LUX.Curve.T1.D3 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T1.D3.pas',
+  LUX.Curve.T1.D1 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T1.D1.pas',
+  LUX.Curve.T1.D2 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T1.D2.pas',
+  LUX.D5 in '_LIBRARY\LUXOPHIA\LUX\LUX.D5.pas',
+  LUX.DN in '_LIBRARY\LUXOPHIA\LUX\LUX.DN.pas',
+  LUX.D2x2 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2x2.pas',
+  LUX.D2x4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2x4.pas',
+  LUX.D2x4x4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2x4x4.pas',
+  LUX.D3x3 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3x3.pas',
+  LUX.D3x4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3x4.pas',
+  LUX.D3x4x4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3x4x4.pas',
+  LUX.D4x4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4x4.pas',
+  LUX.D4x4x4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4x4x4.pas',
+  LUX.FMX.Graphics in '_LIBRARY\LUXOPHIA\LUX\FMX\LUX.FMX.Graphics.pas',
+  LUX.Data.Tree.core in '_LIBRARY\LUXOPHIA\LUX\Data\Tree\LUX.Data.Tree.core.pas',
+  LUX.Data.Tree.Node in '_LIBRARY\LUXOPHIA\LUX\Data\Tree\LUX.Data.Tree.Node.pas',
+  LUX.Data.Tree in '_LIBRARY\LUXOPHIA\LUX\Data\Tree\LUX.Data.Tree.pas',
+  LUX.Draw.Shape.Chart in '_LIBRARY\LUXOPHIA\LUX.Draw\LUX.Draw.Shape.Chart.pas',
+  LUX.Draw.Shape in '_LIBRARY\LUXOPHIA\LUX.Draw\LUX.Draw.Shape.pas',
+  LUX.Draw.Viewer in '_LIBRARY\LUXOPHIA\LUX.Draw\LUX.Draw.Viewer.pas' {DrawViewer: TFrame},
+  LUX.Draw.Scene in '_LIBRARY\LUXOPHIA\LUX.Draw\LUX.Draw.Scene.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ Main.pas
@@ -0,0 +1,61 @@
+﻿unit Main;
+
+interface //####################################################################
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  LUX.Draw.Scene,
+  LUX.Draw.Shape, LUX.Draw.Shape.Chart,
+  LUX.Draw.Viewer;
+
+type
+  TForm1 = class(TForm)
+    DrawViewer1: TDrawViewer;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+    _Scene  :TDrawScene;
+    _Camera :TDrawCamera;
+    _Grid   :TDrawPlots;
+    procedure MakeScene;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation //###############################################################
+
+{$R *.fmx}
+
+procedure TForm1.MakeScene;
+begin
+     _Grid := TDrawPlots.Create( _Scene );
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+     _Scene := TDrawScene.Create;
+
+     _Camera := TDrawCamera.Create( _Scene );
+
+     MakeScene;
+
+     DrawViewer1.Camera := _Camera;
+
+     _Camera.Position := TPointF.Create( 2, 5 );
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+     _Scene.Free;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+end. //#########################################################################
