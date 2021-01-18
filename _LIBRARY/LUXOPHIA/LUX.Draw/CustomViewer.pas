a1f70d17b859be404be89e82c30fc07e4727b291
luxidea <contact@luxidea.net>
2021-01-19 05:16:38 +0900

    CustomViewer フレームテンプレートを追加。

---
+++ _LIBRARY/LUXOPHIA/LUX.Draw/CustomViewer.pas
@@ -0,0 +1,56 @@
+﻿unit CustomViewer;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  LUX.Draw.Scene, LUX.Draw.Viewer, LUX.Draw.Shape;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCustomViewer
+
+     TCustomViewer = class( TDrawViewer )
+     private
+       { private 宣言 }
+       _Scene :TDrawScene;
+     public
+       { public 宣言 }
+       constructor Create( Owner_:TComponent ); override;
+       destructor Destroy; override;
+     end;
+
+implementation //############################################################### ■
+
+{$R *.fmx}
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCustomViewer
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TCustomViewer.Create( Owner_:TComponent );
+begin
+     inherited;
+
+     _Scene := TDrawScene.Create;
+
+     Camera := TDrawCamera.Create( _Scene );
+end;
+
+destructor TCustomViewer.Destroy;
+begin
+     _Scene.Free;
+
+     inherited;
+end;
+
+end. //######################################################################### ■
